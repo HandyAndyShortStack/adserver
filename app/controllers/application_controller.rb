858219ea5e05963a7eb6a37ab035e9e26c9d34ad
Andy Short <andynshort@gmail.com>
2013-11-20 11:17:27 -0800

    added XSS allow header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def blob
+    response.headers["Access-Control-Allow-Origin"] = "*"
     render text: '<div class="blob"><p>this is the blog of text from the ' +
                  'adserver.</p><p>It is HTML!!!!!</p></div>'
   end
